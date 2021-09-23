6f6cb690b5fc5b1502996046cdcf7f157bba406c
lautaronnicolini@gmail.com <lautaronnicolini@gmail.com>
2021-09-23 19:14:02 -0300

    prueba endpoint 2

--- src/main/kotlin/ar/edu/unq/desapp/grupoK/backenddesappapi/BackendDesappApiApplication.kt
+++ src/main/kotlin/ar/edu/unq/desapp/grupoK/backenddesappapi/BackendDesappApiApplication.kt
@@ -2,6 +2,9 @@ package ar.edu.unq.desapp.grupoK.backenddesappapi
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.annotation.Id
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 class BackendDesappApiApplication
