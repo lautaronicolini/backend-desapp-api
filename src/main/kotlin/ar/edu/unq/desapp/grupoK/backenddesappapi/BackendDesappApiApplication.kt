3eb1af0f7fe0cc134ac7f90c8e1536174008adfd
lautaronicolini <lautaronnicolini@gmail.com>
2021-09-23 19:39:36 -0300

    Update BackendDesappApiApplication.kt

--- src/main/kotlin/ar/edu/unq/desapp/grupoK/backenddesappapi/BackendDesappApiApplication.kt
+++ src/main/kotlin/ar/edu/unq/desapp/grupoK/backenddesappapi/BackendDesappApiApplication.kt
@@ -2,18 +2,10 @@ package ar.edu.unq.desapp.grupoK.backenddesappapi
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.data.annotation.Id
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 class BackendDesappApiApplication
 
 fun main(args: Array<String>) {
 	runApplication<BackendDesappApiApplication>(*args)
-}
-
-@RestController
-class Hola { 
-  fun index(): string = "hola!"
 }
