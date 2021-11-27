6883ed932b311043e26f0a48386ea4dc7e08b2aa
lautaronnicolini@gmail.com <lautaronnicolini@gmail.com>
2021-11-27 14:45:37 -0300

    transaction apply endpoint

--- src/main/kotlin/ar/edu/unq/desapp/grupoK/backenddesappapi/app/controllers/TransactionController.kt
+++ src/main/kotlin/ar/edu/unq/desapp/grupoK/backenddesappapi/app/controllers/TransactionController.kt
@@ -1,5 +1,6 @@
 package ar.edu.unq.desapp.grupoK.backenddesappapi.app.controllers
 
+import ar.edu.unq.desapp.grupoK.backenddesappapi.app.domain.TransactionActivity
 import ar.edu.unq.desapp.grupoK.backenddesappapi.app.domain.dto.CreateTransactionDTO
 import ar.edu.unq.desapp.grupoK.backenddesappapi.app.domain.dto.TransactionDetailsDTO
 import ar.edu.unq.desapp.grupoK.backenddesappapi.app.services.TransactionService
@@ -21,5 +22,18 @@ class TransactionController(private val transactionService: TransactionService) 
     fun CreateTransaction(@RequestBody dto:CreateTransactionDTO): ResponseEntity<HttpStatus>{
         transactionService.CreateTransaction(dto)
         return ResponseEntity(HttpStatus.CREATED)
+    }
+
+    @CrossOrigin
+    @RequestMapping("/transaction/all")
+    fun GetAllTransactions(): ResponseEntity<List<TransactionActivity>>{
+        return ResponseEntity(transactionService.GetAllTransactions(), HttpStatus.FOUND)
+    }
+
+    @CrossOrigin
+    @RequestMapping("/transaction/apply")
+    fun ApplyToTransactions(id: Long, userEmail: String): ResponseEntity<HttpStatus>{
+        transactionService.ApplyToTransaction(id, userEmail)
+        return ResponseEntity(HttpStatus.OK)
     }
 }
